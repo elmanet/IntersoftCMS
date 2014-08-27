d8edb18f89b08c57c6f770a17a783ba073987b2d
elmanet <invernet2010@gmail.com>
2014-08-27 11:16:19 -0500

    Agregando Archivos
    Primer Repositorio

---
+++ mgpanel/modules/plantilla-articulos-contenido/modificando.php
@@ -0,0 +1,26 @@
+<?php ob_start();
+
+require_once('../inc/conexion_modules.inc.php'); 
+
+    $contenido=$_POST['contenido'];
+
+
+$updateSQL = sprintf("UPDATE sis_plantilla_articulos SET id_art_cate=%s, titulo_articulo=%s,  alias=%s, contenido=%s, orden=%s,  status=%s WHERE id_articulo=%s",  
+							 
+						GetSQLValueString($_POST['id_art_cate'], "int"),							  
+						GetSQLValueString($_POST['titulo_articulo'], "text"),
+						GetSQLValueString($_POST['alias'], "text"),
+						GetSQLValueString($contenido, "text"),
+						GetSQLValueString($_POST['orden'], "int"),
+                        GetSQLValueString($_POST['status'], "int"),
+                        GetSQLValueString($_POST['id_articulo'], "int"));
+                       
+  mysql_select_db($database_sistemai, $sistemai);
+  $Result1 = mysql_query($updateSQL, $sistemai) or die(mysql_error());
+
+
+$return_loc = "index.php";
+//header("Location: ".$return_loc); 
+
+
+ob_end_flush(); ?>
